309fe272cfa6ccfa959805b8735d12867ebc6d08
Edmundl <32957588+Edmundl@users.noreply.github.com>
2018-06-19 14:42:19 +0800

    Add files via upload

---
+++ 模块化/1.js
@@ -0,0 +1,3 @@
+const mod1=require('./mod'); //  ./是引入自己的模块
+
+console.log(mod1.a,mod1.b,mod1.c);
---
+++ 模块化/mod.js
@@ -0,0 +1,5 @@
+//require  请求：引入模块的
+//module   模块：？
+//exports  输出：？
+
+module.exports={a:12,b:5,c:88};
